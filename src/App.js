b862b6613d8995814302be70e060c243d1593c6f
Nishitha1615 <nishitham0@gmail.com>
2023-04-14 20:11:15 +0530

    1st

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from "./Components/Header";
+import Login from "./Components/Login";
+import Dashboard from "./Components/Dasboard";
+import Error from "./Components/ErrorFile";
+import {Routes,Route} from "react-router-dom";
+import PasswordReset from "./Components/PasswordReset";
+import ForgotPassword from "./Components/ForgotPassword";
+import Register from "./Components/Register"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/dash" element={<Dashboard/>}/>
+        <Route path="*" element={<Error/>}/>
+        <Route path="/password-reset" element={<PasswordReset/>}/>
+        <Route path="/forgotpassword/:id/:token" element={<ForgotPassword/>}/>
+      </Routes>
+
+    </>
   );
+
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Context from './Components/Context/Context';
+import {BrowserRouter} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Context>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
+</Context>
+  
+   
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
